c826edc6982014c4469e1b2158b24c85cc10c35b
mikaijun <mj.tempe443@gmail.com>
2024-05-12 14:25:53 +0900

    Conn.Ping()をなくしてみる

--- pkg/infrastructure/conn.go
+++ pkg/infrastructure/conn.go
@@ -31,17 +31,17 @@ func init() {
 	user := os.Getenv("DBUser")
 	password := os.Getenv("DBPassword")
 	host := os.Getenv("DBHost")
-	// port := os.Getenv("DBPort")
+	port := os.Getenv("DBPort")
 	database := os.Getenv("DBName")
 
 	Conn, err = sql.Open("postgres",
-		fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, "5432", database))
+		fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, database))
 
 	if err != nil {
 		log.Fatal("OpenError: ", err)
 	}
 
-	if err = Conn.Ping(); err != nil {
-		log.Fatal("PingError: ", err)
-	}
+	// if err = Conn.Ping(); err != nil {
+	// 	log.Fatal("PingError: ", err)
+	// }
 }
